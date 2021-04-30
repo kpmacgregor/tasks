e943887071de2be3774f7bfe0104a58064fb8f40
K. P. MacGregor <kpsmacgregor@gmail.com>
2021-04-29 19:49:56 -0700

    Added persistence with localstorage.

--- index.js
+++ index.js
@@ -3,31 +3,54 @@
 const form = document.querySelector('form');
 const pendingTasks = document.querySelector('#pendingTasks');
 const finishedTasks = document.querySelector('#finishedTasks');
+const storage = window.localStorage;
 
-form.addEventListener('submit', (event) => {
-  let taskName = form.newTask.value,
-      li = document.createElement('li'),
+function appendTask(list, taskName, completed) {
+  let li = document.createElement('li'),
       input = document.createElement('input'),
       text = document.createTextNode(taskName);
-  event.preventDefault();
-  input.type = 'checkbox';
-  li.append(input);
-  li.append(text);
-  pendingTasks.append(li);
-  form.newTask.value = '';
+  input.type = "checkbox";
+  input.checked = completed;
   input.addEventListener('change', checkHandler);
+  li.append(input, text);
+  list.append(li);
+}
+
+function loadTasks() {
+  for (i = 0; i < storage.length; ++i) {
+    let taskName = storage.key(i),
+        completed = JSON.parse(storage.getItem(taskName)),
+        parentList = completed ? finishedTasks : pendingTasks;
+    appendTask(parentList, taskName, completed);
+  }
+}
+
+function saveTask(taskName, completed) {
+  storage.setItem(taskName, completed);
+}
+
+form.addEventListener('submit', (event) => {
+  let input = document.querySelector('#newTask'),
+      taskName = input.value;
+  event.preventDefault();
+  saveTask(taskName, false);
+  appendTask(pendingTasks, taskName, false);
+  input.value = '';
 });
 
 function checkHandler(event) {
-  let list = event.target.checked ? finishedTasks : pendingTasks,
-      parentList = !event.target.checked ? finishedTasks : pendingTasks,
-      text = event.target.nextSibling,
-      li = document.createElement('li');
+  let completed = event.target.checked,
+      newList = completed ? finishedTasks : pendingTasks,
+      parentList = !completed ? finishedTasks : pendingTasks,
+      taskName = event.target.nextSibling.data;
+
   parentList.removeChild(event.target.parentElement);
-  li.append(event.target, text);
-  list.append(li);
+  appendTask(newList, taskName, completed);
+  saveTask(taskName, completed);
 }
 
 document.querySelectorAll('input[type=checkbox]').forEach((input) => {
   input.addEventListener('change', checkHandler);
 });
+
+loadTasks();
