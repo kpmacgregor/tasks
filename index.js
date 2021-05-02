746df72fb029a36f13a45d9235057202d787da7b
K. P. MacGregor <kpsmacgregor@gmail.com>
2021-05-02 14:07:18 -0700

    Fixed remove button styling and made it functional.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* index.js */
 
 const storage = window.localStorage;
-const tasks = [];
+let tasks = [];
 
 function makeId(name) {
   return `uid-${name}-${Math.floor(Math.random() * 100000) + Date.now()}`;
@@ -29,25 +29,43 @@ function createTask(name) {
 function registerTask(task) {
   tasks[task.index] = task;
 }
+function unregisterTask(task) {
+  /* using .filter instead of .splice
+     because undefined values left by .splice
+     cause future calls to .find to fail,
+     so they would need to be filtered out anyway.
+  */
+  let newTasks = tasks.filter(t => t !== task);
+  tasks = newTasks;
+}
 
 function saveTask(task) {
   let key = task.id,
       val = JSON.stringify(task);
   storage.setItem(key, val);
 }
+function removeTask(task) {
+  let key = task.id;
+  storage.removeItem(key);
+}
 
 function makeTaskElement(task) {
+  const removeBtnText = document.createTextNode('-');
   let li = document.createElement('li'),
       checkbox = document.createElement('input'),
       label = document.createElement('label'),
-      text = document.createTextNode(task.name);
+      text = document.createTextNode(task.name),
+      removeBtn = document.createElement('button');
   checkbox.type = 'checkbox';
   checkbox.checked = task.completed;
   checkbox.id = task.id;
   label.setAttribute('for', task.id);
   label.append(text);
-  li.append(checkbox, label);
+  removeBtn.setAttribute('data-id', task.id);
+  removeBtn.append(removeBtnText);
+  li.append(checkbox, label, removeBtn);
   checkbox.addEventListener('change', checkHandler);
+  removeBtn.addEventListener('click', removeButtonHandler);
   return li;
 }
 
@@ -65,6 +83,20 @@ function appendTaskElement(task) {
   let parentUl = task.completed ? finishedUl : pendingUL,
       taskElem = getTaskElement(task);
   parentUl.append(taskElem);
+}
+
+function removeTaskElement(task) {
+  let taskElem = getTaskElement(task);
+  taskElem.remove();
+}
+
+function removeButtonHandler(event) {
+  event.preventDefault();
+  let id = event.target.dataset.id,
+      task = tasks.find(t => t.id == id);
+  removeTaskElement(task);
+  removeTask(task);
+  unregisterTask(task);
 }
 
 function checkHandler(event) {
@@ -93,7 +125,6 @@ function loadTasks() {
     registerTask(task);
     appendTaskElement(task);
   }
-  console.log(tasks);
 }
 
 document.querySelector('form').addEventListener('submit', onTaskInput);
