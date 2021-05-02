46eac029e102f1d56db5b5bdfd1e227618fc8b6f
K. P. MacGregor <kpsmacgregor@gmail.com>
2021-05-01 17:56:08 -0700

    Refactored to be object-oriented. Working app.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tasks
+
+A todo app written in javascript, using localStorage for persistence.
+
+## TODO:
+
+- Sort the tasks properly.
+- Provide means to remove tasks.
+- More styling.
--- index.js
+++ index.js
@@ -1,56 +1,95 @@
 /* index.js */
 
-const form = document.querySelector('form');
-const pendingTasks = document.querySelector('#pendingTasks');
-const finishedTasks = document.querySelector('#finishedTasks');
 const storage = window.localStorage;
+const tasks = [];
 
-function appendTask(list, taskName, completed) {
+function makeId(name) {
+  return `uid-${name}-${Math.floor(Math.random() * 100000) + Date.now()}`;
+}
+
+function createTask(name) {
+  let task = {
+    name: name,
+    id: makeId(name),
+    order: tasks.length,
+    completed: false
+  }
+  return task;
+}
+
+function registerTask(task) {
+  tasks[task.order] = task;
+}
+
+function saveTask(task) {
+  let key = task.id,
+      val = JSON.stringify(task);
+  storage.setItem(key, val);
+}
+
+function makeTaskElement(task) {
   let li = document.createElement('li'),
-      input = document.createElement('input'),
-      text = document.createTextNode(taskName);
-  input.type = "checkbox";
-  input.checked = completed;
-  input.addEventListener('change', checkHandler);
-  li.append(input, text);
-  list.append(li);
+      checkbox = document.createElement('input'),
+      label = document.createElement('label'),
+      text = document.createTextNode(task.name);
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  checkbox.id = task.id;
+  label.setAttribute('for', task.id);
+  label.append(text);
+  li.append(checkbox, label);
+  checkbox.addEventListener('change', checkHandler);
+  return li;
+}
+
+function getTaskElement(task) {
+  let input = document.querySelector(`#${task.id}`);
+  if (input == null) {
+    return makeTaskElement(task);
+  }
+  return input.parentElement;
+}
+
+function appendTaskElement(task) {
+  const finishedUl = document.querySelector('#finishedTasks'),
+        pendingUL = document.querySelector('#pendingTasks');
+  let parentUl = task.completed ? finishedUl : pendingUL,
+      taskElem = getTaskElement(task);
+  parentUl.append(taskElem);
+}
+
+function checkHandler(event) {
+  let input = event.target,
+      id = input.id,
+      task = tasks.find(t => t.id == id);
+  task.completed = input.checked;
+  saveTask(task);
+  appendTaskElement(task);
+}
+
+function onTaskInput(event) {
+  const input = document.querySelector('#newTask');
+  let task = createTask(input.value);
+  registerTask(task);
+  saveTask(task);
+  appendTaskElement(task);
+  event.preventDefault();
+  input.value = '';
 }
 
 function loadTasks() {
   for (i = 0; i < storage.length; ++i) {
-    let taskName = storage.key(i),
-        completed = JSON.parse(storage.getItem(taskName)),
-        parentList = completed ? finishedTasks : pendingTasks;
-    appendTask(parentList, taskName, completed);
+    let key = storage.key(i),
+        task = JSON.parse(storage.getItem(key));
+    registerTask(task);
+    appendTaskElement(task);
   }
 }
 
-function saveTask(taskName, completed) {
-  storage.setItem(taskName, completed);
-}
-
-form.addEventListener('submit', (event) => {
-  let input = document.querySelector('#newTask'),
-      taskName = input.value;
-  event.preventDefault();
-  saveTask(taskName, false);
-  appendTask(pendingTasks, taskName, false);
-  input.value = '';
-});
-
-function checkHandler(event) {
-  let completed = event.target.checked,
-      newList = completed ? finishedTasks : pendingTasks,
-      parentList = !completed ? finishedTasks : pendingTasks,
-      taskName = event.target.nextSibling.data;
-
-  parentList.removeChild(event.target.parentElement);
-  appendTask(newList, taskName, completed);
-  saveTask(taskName, completed);
-}
-
-document.querySelectorAll('input[type=checkbox]').forEach((input) => {
-  input.addEventListener('change', checkHandler);
-});
-
+document.querySelector('form').addEventListener('submit', onTaskInput);
 loadTasks();
+
+function clear() {
+  /* for testing */
+  window.localStorage.clear();
+}
